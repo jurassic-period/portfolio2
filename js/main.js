de122a5a74a5ea770367ec0f8e2bbb2ecb6144ab
Yurii <ya.donny@yandex.ru>
2019-02-09 14:14:21 +0300

    a lot of changes with animation

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
 //Call Wow JS libruary
-new WOW().init();
+wow = new WOW(
+    {
+    boxClass:     'wow',      // default
+    animateClass: 'animated', // default
+    offset:       200,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  )
+  wow.init();
+
+  //Click to nav button
+  $(document).ready(function(){
+	// плавное перемещение страницы к нужному блоку
+	$(".click").click(function () {
+		elementClick = $(this).attr("href");
+		destination = $(elementClick).offset().top;
+		$("body,html").animate({scrollTop: destination }, 1400);
+	});
+});
